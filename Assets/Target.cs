b8d228667c5a09e5444b967ae79778d15f4423f3
SaZaNKod <sazankod@gmail.com>
2019-08-10 20:12:00 +0530

    First Commit

---
+++ Assets/Target.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+
+    public void TakeDamage (float amount)
+    {
+        health -= amount; //sub amout from health
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+
+    }
+}
